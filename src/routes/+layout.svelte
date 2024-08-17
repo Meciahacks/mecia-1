13ebd11b74aa42e020218eecf3b2c96a8fdd184c
cmsusergit <cmsusergit@gmail.com>
2024-08-17 16:29:25 +0530

    commit done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,51 @@
 <script>
-	import Header from './Header.svelte';
 	import '../app.css';
-</script>
+	
+	import { authStore, pb } from '../auth.js';
+	import { goto } from '$app/navigation';
+  
+	authStore.subscribe((store) => {
+    let user = store.user;
+		if (!store.isLoggedIn) {
+		goto('/login');
+		}
+	});
+
+  	function logout() {
+	  pb.authStore.clear();
+	  goto('/login');
+	}
+  </script>
+
 
 <div class="app">
-	<Header />
+	<header>	
+	  <nav>
+		  <a href="/">Home</a>
+		  {#if $authStore.isLoggedIn}
+			  <a href="/">Dashboard</a>
+			  <a href="/logout" on:click={logout}>Logout</a>
+		  {:else}
+			  <a href="/login">Login</a>
+			  <a href="/signup">Signup</a>
+		  {/if}
+		</nav>
+  </header>
 
-	<main>
+
+
+
+
+  <main>
 		<slot />
 	</main>
-
 	<footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+<style>	
 </style>
+
+
+
+
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,59 @@
+<script>	
+	import { pb, authStore } from '../../auth.js'
+
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	let email = '';
+	let password = '';
+	let error = '';
+	async function login() {
+	  try {		
+		await pb.collection('users').authWithPassword(email, password);
+		goto('/');
+
+	} catch (err) {
+		error = 'Invalid login credentials';
+	  }
+	}
+	onMount(() => {
+	  authStore.subscribe((store) => {
+		if (store.isLoggedIn) {
+		  goto('/dashboard');
+		}
+	  });
+	});
+  </script>
+
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="Login" />
 </svelte:head>
+<div>
+  <h2>Login</h2>
+  {#if error}
+	<p style="color: red;">{error}</p>
+  {/if}
 
-<div class="text-column">
-	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+  <form on:submit|preventDefault={login} class="container">
+	<label class="form-control w-full">
+		<div class="label">
+		  <span class="label-text">Email</span>
+		</div>
+		<input type="text" placeholder="Email" class="input input-bordered w-full"  bind:value={email} required/>		
+	</label>
+	<label class="form-control w-full">
+		<div class="label">
+		  <span class="label-text">Password</span>
+		</div>
+		<input type="password" placeholder="Password" class="input input-bordered w-full"  bind:value={password} required/>		
+	</label>
+	
+	<div class="w-full my-4">
+		<button type="submit" class="btn btn-neutral btn-wide w-full">Login</button>
+	</div>
+  </form>
 </div>
+
+
+
